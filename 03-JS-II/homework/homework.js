2795280590fabc12c7c0ac6f762974d1c77345ae
EmiCon97 <derleuchtturm97@gmail.com>
2021-11-25 21:02:29 -0300

    ninth commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -155,7 +155,7 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
   for (i=2; i<numero; i++) {
-    if (numero%i===0) {
+    if (numero%i!==0) {
       return true
     }
   }
